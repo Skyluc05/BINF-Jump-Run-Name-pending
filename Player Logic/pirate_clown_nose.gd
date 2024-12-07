6c574cccea1caa33948dfd290cfe2e32f4def6a0
Skyluc05 <152433467+Skyluc05@users.noreply.github.com>
2024-12-07 22:11:21 +0100

    Jump und run stuff coding update (noch nicht fertig)

--- Player Logic/pirate_clown_nose.gd
+++ Player Logic/pirate_clown_nose.gd
@@ -7,25 +7,33 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	
 	if is_on_floor():
-		anim.play("Idle")
-	
-	if not is_on_floor():
+			anim.play("Idle")
+	if not is_on_floor():	
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor():
+			jump()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-		anim.stop()
+	if Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
+		var direction := Input.get_axis("Left", "Right")
+		run(direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+	
+
+func jump():
+	anim.play("Jump")
+	velocity.y = JUMP_VELOCITY
+	
+	
+func run(direction):
+	if is_on_floor():
+		anim.play("Run")
+	
+	velocity.x = direction * SPEED
+	
