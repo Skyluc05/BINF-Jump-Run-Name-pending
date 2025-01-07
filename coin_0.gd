da313c918b1dd19f506269e9523fc9ad0e07bae7
JutanDragon <154429364+JutanDragon@users.noreply.github.com>
2025-01-07 22:21:13 +0100

    Coins plaziert und animationen Aktiviert

---
+++ coin_0.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+var coin_is_da = true
+var coin_collected = false
+
+func _process(delta: float) -> void:
+	if coin_collected == true:
+		play("Coin WEG")
+	
+	elif coin_is_da == true:
+		play("Coin")
+	else:
+		play("Coin Collect")
+		coin_collected = true
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	coin_is_da = false
